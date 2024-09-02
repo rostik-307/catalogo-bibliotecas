77d280104f02a8f2b2e70c1d40182b3cccf89ffa
curso02 <curso02@nomail.com>
2024-09-02 09:32:25 +0200

    Ajuste router, creación componente ListadoItem

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -12,7 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/listado">Listado</RouterLink>
+        <RouterLink to="/"></RouterLink>
       </nav>
     </div>
   </header>
