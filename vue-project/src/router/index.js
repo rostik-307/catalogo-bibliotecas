0dbbd885339416f0f2f95840a84aae4d3612f8c1
curso14 <curso14@nomail.com>
2024-09-02 11:35:04 +0200

    Añadimos a index.js y app.ve InsertarItem

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
 import ListadoItem from '@/components/ListadoItem.vue'
+import InsertarItem from '@/components/InsertarItem.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +16,11 @@ const router = createRouter({
       path: '/listado',
       name: 'listado',
      component: ListadoItem
-      
+    },
+    {
+      path: '/insertar',
+      name: 'insertar',
+      component: InsertarItem
     }
   ]
 })
