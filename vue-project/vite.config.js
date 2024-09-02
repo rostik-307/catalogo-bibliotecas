d464aeefc57d90b356d4eb1ecd16c5a45cf2b4a6
curso02 <curso02@nomail.com>
2024-09-02 11:34:11 +0200

    Cambios permisos CORS en ListadoItem x 2

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +11,23 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/listado': {
+        target: 'http://localhost:8080',  
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+      '/insertar': {
+        target: 'http://localhost:8080', 
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+});
