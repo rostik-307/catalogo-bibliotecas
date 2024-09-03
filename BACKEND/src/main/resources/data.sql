4a3667df8f836effad267bff364317ab96138cf4
rostik-307 <rkonchenko@cic.es>
2024-09-03 10:23:29 +0200

    sql patch

--- BACKEND/src/main/resources/data.sql
+++ BACKEND/src/main/resources/data.sql
@@ -1,13 +1,13 @@
--- Insertar datos de ejemplo en la tabla ITEM (El ID se genera automáticamente)
-INSERT INTO ITEM (name, details, release_year) VALUES ('Thriller', 'Álbum de Michael Jackson', 1982);
-INSERT INTO ITEM (name, details, release_year) VALUES ('The Dark Side of the Moon', 'Álbum de Pink Floyd', 1973);
-INSERT INTO ITEM (name, details, release_year) VALUES ('Star Wars', 'Película de George Lucas', 1977);
-INSERT INTO ITEM (name, details, release_year) VALUES ('The Godfather', 'Película de Francis Ford Coppola', 1972);
-INSERT INTO ITEM (name, details, release_year) VALUES ('Cien Años de Soledad', 'Una novela de Gabriel García Márquez', 1967);
-
 -- Insertar datos de ejemplo en la tabla CATEGORY (El ID se genera automáticamente)
 -- Aquí, item_id hace referencia al ID del ITEM al que esta categoría está asociada.
-INSERT INTO CATEGORY (name, details, item_id) VALUES ('CD', 'Discos compactos de música', 1);
-INSERT INTO CATEGORY (name, details, item_id) VALUES ('VHS', 'Cintas de video VHS', 2);
-INSERT INTO CATEGORY (name, details, item_id) VALUES ('Libro', 'Libros de diferentes géneros', 3);
-INSERT INTO CATEGORY (name, details, item_id) VALUES ('Revista', 'Revistas de diferentes temas', 4);
+INSERT INTO CATEGORY (id, name, details) VALUES (1, 'CD', 'Discos compactos de música');
+INSERT INTO CATEGORY (id, name, details) VALUES (2, 'VHS', 'Cintas de video VHS');
+INSERT INTO CATEGORY (id, name, details) VALUES (3, 'Libro', 'Libros de diferentes géneros');
+INSERT INTO CATEGORY (id, name, details) VALUES (4, 'Revista', 'Revistas de diferentes temas');
+
+-- Insertar datos de ejemplo en la tabla ITEM (El ID se genera automáticamente)
+INSERT INTO ITEM (name, details, release_year, categoria_id) VALUES ('Thriller', 'Álbum de Michael Jackson', 1982, 2);
+INSERT INTO ITEM (name, details, release_year, categoria_id) VALUES ('The Dark Side of the Moon', 'Álbum de Pink Floyd', 1973, 2);
+INSERT INTO ITEM (name, details, release_year, categoria_id) VALUES ('Star Wars', 'Película de George Lucas', 1977, 1);
+INSERT INTO ITEM (name, details, release_year, categoria_id) VALUES ('The Godfather', 'Película de Francis Ford Coppola', 1972, 4);
+INSERT INTO ITEM (name, details, release_year, categoria_id) VALUES ('Cien Años de Soledad', 'Una novela de Gabriel García Márquez', 1967, 3);
