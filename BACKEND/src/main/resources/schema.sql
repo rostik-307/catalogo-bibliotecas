d28f33a07feba799f2ebc9c54f7f8011dc0019e4
curso02 <curso02@nomail.com>
2024-09-03 10:15:45 +0200

    Correcciones data.sql y schema.sql

--- BACKEND/src/main/resources/schema.sql
+++ BACKEND/src/main/resources/schema.sql
@@ -1,16 +1,16 @@
--- Crear la tabla CATEGORY
-CREATE TABLE CATEGORY (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    details VARCHAR(255)
-);
-
--- Crear la tabla ITEM
+-- Crear la tabla ITEM (equivalente a Expediente)
 CREATE TABLE ITEM (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     details VARCHAR(255),
-    release_year INTEGER,
-    categoria_id BIGINT,
-    FOREIGN KEY (categoria_id) REFERENCES CATEGORY(id)
+    release_year INTEGER
+);
+
+-- Crear la tabla CATEGORY (equivalente a Documento)
+CREATE TABLE CATEGORY (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    details VARCHAR(255),
+    item_id BIGINT,
+    FOREIGN KEY (item_id) REFERENCES ITEM(id) ON DELETE CASCADE
 );
